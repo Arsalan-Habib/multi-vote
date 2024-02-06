f4c665f83651cac842fba379e50e263a64ed311e
Arsalan-Habib <syedarsalanhabib@gmail.com>
2024-02-07 02:13:34 +0500

    feat: disabled styles updates

--- src/app/add-vote/page.tsx
+++ src/app/add-vote/page.tsx
@@ -58,7 +58,7 @@ export default function AddVote() {
                     className={`mt-4 px-6 py-2 font-semibold rounded-lg ${
                         isFormComplete
                             ? "bg-blue-500 hover:bg-blue-600 text-white"
-                            : "bg-blue-200 text-blue-800"
+                            : "bg-gray-200 text-gray-400"
                     }`}
                     disabled={!isFormComplete}
                 >
