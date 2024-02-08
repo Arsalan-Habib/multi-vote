d173932e2222344428737b95c8eb2ee17e85898b
Arsalan-Habib <syedarsalanhabib@gmail.com>
2024-02-08 22:45:22 +0500

    refactor: enable timestamps for vote schema

--- src/models/Vote.js
+++ src/models/Vote.js
@@ -1,19 +1,24 @@
 // models/Vote.js
 import mongoose from "mongoose";
 
-const voteSchema = new mongoose.Schema({
-    voter: {
-        type: String,
-        required: true,
-        unique: true, // Ensure that each voter can only have one vote document
-    },
-    applicants: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Applicant",
+const voteSchema = new mongoose.Schema(
+    {
+        voter: {
+            type: String,
             required: true,
+            unique: true, // Ensure that each voter can only have one vote document
         },
-    ],
-});
+        applicants: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Applicant",
+                required: true,
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
 
 export default mongoose.models.Vote || mongoose.model("Vote", voteSchema);
