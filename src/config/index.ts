633587e5adfe523600c6ec7d4dadd6069104d867
Arsalan-Habib <syedarsalanhabib@gmail.com>
2024-02-26 19:36:05 +0500

    chore: update voting end time

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,1 @@
-export const votingEndTime = 1707764400000; // Tuesday, February 13, 2024 12:00:00 AM GMT+05:00
+export const votingEndTime = 1709060400000; // Tuesday, February 13, 2024 12:00:00 AM GMT+05:00
