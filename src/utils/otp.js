1ada55ea35cde4e32c7ba6ea22b864dd7e3dde96
Arsalan-Habib <syedarsalanhabib@gmail.com>
2024-07-24 02:57:31 +0500

    fix: comment twilio otp code

--- src/utils/otp.js
+++ src/utils/otp.js
@@ -1,32 +1,34 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const serviceId = process.env.TWILIO_SERVICE_ID;
-const client = require("twilio")(accountSid, authToken);
-import { phone } from "phone";
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const serviceId = process.env.TWILIO_SERVICE_ID;
+// const client = require("twilio")(accountSid, authToken);
+// import { phone } from "phone";
 
 export const sendOTP = async (to) => {
-    const { phoneNumber } = phone(to);
+  //   const { phoneNumber } = phone(to);
 
-    const verification = await client.verify.v2
-        .services(serviceId)
-        .verifications.create({
-            to: phoneNumber,
-            channel: "sms",
-        });
-    return verification;
+  // const verification = await client.verify.v2
+  //     .services(serviceId)
+  //     .verifications.create({
+  //         to: phoneNumber,
+  //         channel: "sms",
+  //     });
+  // return verification;
+  return { status: "approved" };
 };
 
 export const verifyOTP = async (to, code) => {
-    const { phoneNumber } = phone(to);
+  // const { phoneNumber } = phone(to);
 
-    const verification = await client.verify.v2
-        .services(serviceId)
-        .verificationChecks.create({
-            to: phoneNumber,
-            code,
-        });
-    return verification;
+  // const verification = await client.verify.v2
+  //     .services(serviceId)
+  //     .verificationChecks.create({
+  //         to: phoneNumber,
+  //         code,
+  //     });
+  // return verification;
+  return { status: "approved" };
 };
